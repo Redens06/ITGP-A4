0af79e393e8c3e95db00307a108eb64842c5a3cc
Redens06 <giulianozuccara06@gmail.com>
2026-05-03 17:07:24 +1000

    added random ene logic
    added logic to allow enemies to spawn at random

--- assignment-4-itgp/Scenes/world.gd
+++ assignment-4-itgp/Scenes/world.gd
@@ -9,3 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+var enemy = preload("res://Scenes/enemy.tscn")
+
+func _on_spawn_timer_timeout():
+	var enemy_instance = enemy.instantiate()
+	add_child(enemy_instance)
+	enemy_instance.position = $spawnlocation.position
+
+	var nodes = get_tree().get_nodes_in_group("spawn")
+	var node = nodes.pick_random()
+	var position = node.position  
+	$spawnlocation.position = position
