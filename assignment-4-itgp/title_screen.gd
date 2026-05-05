1388b40b6e6aba379c041a0e02ed08957979d0e2
Bones-Malone <67958852+Bones-Malone@users.noreply.github.com>
2026-05-05 12:45:10 +1000

    added title screen

---
+++ assignment-4-itgp/title_screen.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$Camera2D.position.x = get_local_mouse_position().x / 20
+	$Camera2D.position.y = get_local_mouse_position().y / 20
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
