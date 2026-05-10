51f6377f46ceea9981eabfb4461c6e0461e7adfa
Bones-Malone <67958852+Bones-Malone@users.noreply.github.com>
2026-05-11 09:17:56 +1000

    replaced the 9 spawn points with a spawn line

--- assignment-4-itgp/Script/world.gd
+++ assignment-4-itgp/Script/world.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-var spawnPoints
+#var spawnPoints
+var spawnLine : PathFollow2D
 
 var enemy = preload("res://Scenes/enemy.tscn")
 
@@ -12,7 +13,8 @@ var enemy_list = [
 ]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawnPoints = get_tree().get_nodes_in_group("spawn")
+	#spawnPoints = get_tree().get_nodes_in_group("spawn")
+	spawnLine = get_tree().get_first_node_in_group("spawner")
 
 func _on_spawn_timer_timeout():
 	#var enemy_instance = enemy.instantiate()
@@ -20,6 +22,7 @@ func _on_spawn_timer_timeout():
 	#enemy_instance.position = $spawnlocation.position
 	
 	var scene = enemy_list.pick_random().instantiate()
-	var pickedSpawn = spawnPoints.pick_random()
-	scene.position = pickedSpawn.global_position 
+	#var pickedSpawn = spawnPoints.pick_random()
+	spawnLine.progress_ratio = randf_range(0,1)
+	scene.position = spawnLine.global_position
 	add_child(scene)
