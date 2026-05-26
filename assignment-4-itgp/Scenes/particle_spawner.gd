e347e1a1d37dfa7c8717e1691eeee012c20e2ea7
Bones-Malone <67958852+Bones-Malone@users.noreply.github.com>
2026-05-26 12:38:16 +1000

    added light orb particles

--- assignment-4-itgp/Scenes/particle_spawner.gd
+++ assignment-4-itgp/Scenes/particle_spawner.gd
@@ -4,8 +4,13 @@ func playParticle(type : String):
 	match type:
 		"explosion":
 			$fireballExplosion.emitting = true
+		"lightShatter":
+			$lightShatter.emitting = true
 		_:
 			$fireballExplosion.emitting = true
 
 func _on_fireball_explosion_finished() -> void:
+	queue_free()
+
+func _on_light_shatter_finished() -> void:
 	queue_free()
