3ca6a17324c69f08523fa8a14c3750e0dda0ab91
JonathanWhi <105917529@student.swin.edu.au>
2026-05-26 14:12:59 +1000

    Creation of scene bounds and transition to dungeon

--- assignment-4-itgp/Script/global.gd
+++ assignment-4-itgp/Script/global.gd
@@ -1,3 +1,18 @@
 extends Node
 
 var player_current_attack = false 
+
+var current_scene = "world" #world Dungeon 
+var transition_scene = false	 
+
+var player_exit_dungeon_posx = 0
+var player_exit_dungeon_posy = 0
+var player_start_posx = 0
+var player_start_posy = 0 
+	
+func finish_changescenes():
+		if transition_scene == true:
+			transition_scene = false
+			if current_scene == "world":
+				current_scene = "dungeon"
+			else: current_scene = "world"
--- assignment-4-itgp/Script/world.gd
+++ assignment-4-itgp/Script/world.gd
@@ -38,3 +38,23 @@ func _on_goblin_timer_timeout():
 	pass # Replace with function body.
 	#goblin respawn logic will go here
 	#will add once they have sprites 
+	
+func _physics_process(delta):
+	change_scene()
+
+func _on_dungeontransitionpoint_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		global.transition_scene = true
+
+
+func _on_dungeontransitionpoint_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		global.transition_scene = false
+		
+func change_scene():
+	if global.transition_scene == true:
+		if global.current_scene == "world":
+			get_tree().change_scene_to_file("res://Scenes/dungeon.tscn")
+			global.finsih_changingscenes()
+			
+			
